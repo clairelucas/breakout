edd8cf733ca5d85f46160b5127472ad951be3f12
clairelucas <72223751+clairelucas@users.noreply.github.com>
2020-10-02 14:20:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var links = [
  "https://teams.microsoft.com/l/meetup-join/19%3ameeting_YzNmZDlmY2YtNzdkOS00ZDRjLTk2MjktMWYwZmYwODVlYzFh%40thread.v2/0?context=%7b%22Tid%22%3a%228370cf14-16f3-4c16-b83c-724071654356%22%2c%22Oid%22%3a%221e71dcf0-d665-41d7-b944-2af30aa1a491%22%7d",
               "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZjE3ZWJhYjgtZTMzZS00ODkzLWI5OWQtMjdmYjM3OGViNmIz%40thread.v2/0?context=%7b%22Tid%22%3a%228370cf14-16f3-4c16-b83c-724071654356%22%2c%22Oid%22%3a%221e71dcf0-d665-41d7-b944-2af30aa1a491%22%7d",
+ "http://youtube.com",
+ "http://facebook.com",
 ];
 var link = links[Math.floor(Math.random() * links.length)];
 window.location = link;
